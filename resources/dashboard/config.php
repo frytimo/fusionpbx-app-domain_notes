e8a9260702c2a6f1606579688b16cc92521d6f30
Tim Fry <tim@fusionpbx.com>
2026-07-03 13:58:00 +0000

    Refactor dashboard configuration to add domain notes widget on upgrade

--- resources/dashboard/config.php
+++ resources/dashboard/config.php
@@ -1,63 +1,39 @@
 <?php
 
-/*
- * FusionPBX
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is FusionPBX
- *
- * The Initial Developer of the Original Code is
- * Mark J Crane <markjcrane@fusionpbx.com>
- * Portions created by the Initial Developer are Copyright (C) 2008-2024
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Mark J Crane <markjcrane@fusionpbx.com>
- * Tim Fry <tim@fusionpbx.com>
- */
-
-$array['dashboard'][$x]['dashboard_uuid'] = '2f6f417d-c910-4fbd-928a-364177f0a185';
-$array['dashboard'][$x]['dashboard_name'] = 'Notes';
-$array['dashboard'][$x]['dashboard_path'] = 'app/notes/resources/dashboard/notes.php';
-$array['dashboard'][$x]['dashboard_icon'] = '';
-$array['dashboard'][$x]['dashboard_url'] = '';
-$array['dashboard'][$x]['dashboard_target'] = 'self';
-$array['dashboard'][$x]['dashboard_width'] = '';
-$array['dashboard'][$x]['dashboard_height'] = '';
-$array['dashboard'][$x]['dashboard_content'] = '';
-$array['dashboard'][$x]['dashboard_content_text_align'] = '';
-$array['dashboard'][$x]['dashboard_content_details'] = '';
-$array['dashboard'][$x]['dashboard_chart_type'] = '';
-$array['dashboard'][$x]['dashboard_label_enabled'] = 'true';
-$array['dashboard'][$x]['dashboard_label_text_color'] = '';
-$array['dashboard'][$x]['dashboard_label_text_color_hover'] = '';
-$array['dashboard'][$x]['dashboard_label_background_color'] = '';
-$array['dashboard'][$x]['dashboard_label_background_color_hover'] = '';
-$array['dashboard'][$x]['dashboard_number_background_color'] = '';
-$array['dashboard'][$x]['dashboard_number_text_color'] = '';
-$array['dashboard'][$x]['dashboard_number_text_color_hover'] = '';
-$array['dashboard'][$x]['dashboard_background_color'] ='';
-$array['dashboard'][$x]['dashboard_background_color_hover'] = '';
-$array['dashboard'][$x]['dashboard_detail_background_color'] = '';
-$array['dashboard'][$x]['dashboard_column_span'] = '1';
-$array['dashboard'][$x]['dashboard_row_span'] = '2';
-$array['dashboard'][$x]['dashboard_details_state'] = 'hidden';
-$array['dashboard'][$x]['dashboard_order'] = '190';
-$array['dashboard'][$x]['dashboard_enabled'] = 'true';
-$array['dashboard'][$x]['dashboard_description'] = 'Notes for Domains';
-
-
+//domain notes text area
+$array['dashboard_widgets'][$x]['dashboard_uuid'] = '3e2cbaa4-2bec-41b2-a626-999a59b8b19c';
+$array['dashboard_widgets'][$x]['dashboard_widget_uuid'] = '3a7f0e6b-6912-4c6f-94e5-6aa564ad9205';
+$array['dashboard_widgets'][$x]['widget_name'] = 'Notes';
+$array['dashboard_widgets'][$x]['widget_path'] = '/app/domain_notes/resources/dashboard/domain_notes.php';
+$array['dashboard_widgets'][$x]['widget_icon'] = '';
+$array['dashboard_widgets'][$x]['widget_url'] = '';
+$array['dashboard_widgets'][$x]['widget_target'] = 'self';
+$array['dashboard_widgets'][$x]['widget_width'] = '';
+$array['dashboard_widgets'][$x]['widget_height'] = '';
+$array['dashboard_widgets'][$x]['widget_content'] = '';
+$array['dashboard_widgets'][$x]['widget_content_text_align'] = '';
+$array['dashboard_widgets'][$x]['widget_content_details'] = '';
+$array['dashboard_widgets'][$x]['widget_chart_type'] = '';
+$array['dashboard_widgets'][$x]['widget_label_enabled'] = 'true';
+$array['dashboard_widgets'][$x]['widget_label_text_color'] = '';
+$array['dashboard_widgets'][$x]['widget_label_text_color_hover'] = '';
+$array['dashboard_widgets'][$x]['widget_label_background_color'] = '';
+$array['dashboard_widgets'][$x]['widget_label_background_color_hover'] = '';
+$array['dashboard_widgets'][$x]['widget_number_background_color'] = '';
+$array['dashboard_widgets'][$x]['widget_number_text_color'] = '';
+$array['dashboard_widgets'][$x]['widget_number_text_color_hover'] = '';
+$array['dashboard_widgets'][$x]['widget_background_color'] ='';
+$array['dashboard_widgets'][$x]['widget_background_color_hover'] = '';
+$array['dashboard_widgets'][$x]['widget_detail_background_color'] = '';
+$array['dashboard_widgets'][$x]['widget_column_span'] = '4';
+$array['dashboard_widgets'][$x]['widget_row_span'] = '2';
+$array['dashboard_widgets'][$x]['widget_details_state'] = 'visible';
+$array['dashboard_widgets'][$x]['widget_order'] = '0';
+$array['dashboard_widgets'][$x]['widget_enabled'] = 'true';
+$array['dashboard_widgets'][$x]['widget_description'] = 'Notes for Domains';
 $y = 0;
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_group_uuid'] = '6fb4d5c8-c95d-400f-b96c-edc044571a92';
-$array['dashboard'][$x]['dashboard_groups'][$y]['dashboard_uuid'] = 'f9334ac5-f9af-4fa9-b6df-bffe6df194f5';
-$array['dashboard'][$x]['dashboard_groups'][$y]['group_name'] = 'superadmin';
+$array['dashboard_widgets'][$x]['dashboard_widget_groups'][$y]['dashboard_uuid'] = '3e2cbaa4-2bec-41b2-a626-999a59b8b19c';
+$array['dashboard_widgets'][$x]['dashboard_widget_groups'][$y]['dashboard_widget_group_uuid'] = '73c5fd89-b627-4eb9-8ba8-fd32d23f04c9';
+$array['dashboard_widgets'][$x]['dashboard_widget_groups'][$y]['dashboard_widget_uuid'] = '3a7f0e6b-6912-4c6f-94e5-6aa564ad9205';
+$array['dashboard_widgets'][$x]['dashboard_widget_groups'][$y]['group_name'] = 'superadmin';
+$y++;
